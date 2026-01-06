adb58ac15573c9935f38efe4763b759ca4922282
AnjolaOsoba <anjolaoluwao7@nycstudents.net>
2026-01-06 19:09:44 +0000

    edstdz

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,21 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+
+function image1() {
+  document.getElementById("highlight").innerHTML = "Funny random picture";
+}
+
+function image2() {
+  document.getElementById("highlight").innerHTML = "Rare funny image";
+}
+
+function image3() {
+  document.getElementById("highlight").innerHTML = "Random internet image";
+}
+
+function image4() {
+  document.getElementById("highlight").innerHTML = "Galaxy puzzle image";
+}
+
+function image5() {
+  document.getElementById("highlight").innerHTML = "Artistic photograph";
+}
